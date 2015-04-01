67dfbc743b4c46f9f5257beab3b281b3e74ab093
Jacek Siciarek <jacek.siciarek@escore.pl>
2015-04-01 15:19:11 +0200

    Knp Paginator added

---
+++ src/Application/MainBundle/Controller/ArticleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Application\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/article")
+ */
+class ArticleController extends Controller {
+
+    /**
+     * @Route("/list", name="article.list")
+     * @Template()
+     */
+    public function listAction(Request $request) {
+        
+        $qb = $this->getDoctrine()->getManager()->createQueryBuilder();
+        
+        $query = $qb
+                ->from('\Application\MainBundle\Entity\Article', 'a')
+                ->select('a')
+                ->getQuery()
+        ;
+
+        $paginator = $this->get('knp_paginator');
+        $page = $request->query->get('page', 1);
+        $limit = $this->container->getParameter('pager_limit');
+
+        $items = $paginator->paginate($query, $page, $limit);
+
+        return [
+            'items' => $items,
+        ];
+    }
+
+}

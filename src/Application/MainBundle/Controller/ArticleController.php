e2bb185b61754118b97ecf025856afc379e81881
Jacek Siciarek <jacek.siciarek@escore.pl>
2015-04-15 13:41:35 +0200

    Add secure controller

--- src/Application/MainBundle/Controller/ArticleController.php
+++ src/Application/MainBundle/Controller/ArticleController.php
@@ -2,12 +2,14 @@
 
 namespace Application\MainBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
+ * @Security("has_role('ROLE_USER')")
  * @Route("/article")
  */
 class ArticleController extends Controller {

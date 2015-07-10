c7d0f0cedb0aa9faff40db502e3bbdbee306afaf
Jacek Siciarek <jacek.siciarek@escore.pl>
2015-07-10 16:30:39 +0200

    Image - grayscale converter

--- go.php
+++ go.php
@@ -1,7 +1,7 @@
 <?php
 
 define('GNUPLOT', 'gnuplot');
-define('SAMPLING', 512);
+define('SAMPLING', 64);
 define('M_2PI', 2 * M_PI);
 
 $count = 5;
@@ -18,7 +18,7 @@ while(count($frequencies) < $count) {
 	$frequencies[] = $r;
 }
 
-$frequencies = range(1, 17);
+$frequencies = [3, 7];
 
 sort($frequencies);
 
@@ -40,7 +40,7 @@ function generateSamples($freq, $duration = 1) {
 $time = [];
 
 foreach($frequencies as $f) {
-	$time[] = generateSamples($f);
+    $time[] = generateSamples($f);
 }
 
 $x = [];
@@ -55,6 +55,20 @@ for ($i = 0; $i < count($time[0]); $i++) {
     $x[] = $sample;
 }
 
+
+//$x = [];
+//for($i = 0; $i < 512; $i++) {
+//    
+//    if($i < 62) {
+//        $x[] = (rand(0, 1) ? -1 : 1) * 0.05 * floatval(rand(0, $i % 32));
+//        continue;
+//    }
+//    
+//    $x[] = 0;
+//}
+
+// ----- Koniec generowania 
+
 $N = count($x);
 $m = 0;
 $j = 1;
@@ -66,7 +80,7 @@ for ($m = 0; $m < $N; $m++) {
 
     for ($n = 0; $n < $N; $n++) {
         $coef = (M_2PI * $n * $m) / $N;
-        $sum += $x[$n] * (cos($coef) - sin($coef));
+        $sum += $x[$n] * cos($coef) - $x[$n] * sin($coef);
     }
 
     $X[$m] = abs($sum);
@@ -97,7 +111,7 @@ set xtics 2
 set size 1, 0.5
 set origin 0, 0
 set xrange [0:$xmax]
-plot "frequency.dat" notitle with impulses 
+plot "frequency.dat" notitle with lines
 
 unset multiplot
 pause($delay)
---
+++ web/stash/go.php
@@ -0,0 +1,141 @@
+<?php
+
+$filename = __DIR__ . DIRECTORY_SEPARATOR . 'cballs.png';
+$trgfilename = __DIR__ . DIRECTORY_SEPARATOR . 'xcballs.png';
+
+function getInfo($filename) {
+    $info = getimagesize($filename);
+
+    $w = $info[0];
+    $h = $info[1];
+
+    return [
+        'width' => $w,
+        'height' => $h,
+    ];
+}
+
+function getPixels($filename) {
+
+    $info = getInfo($filename);
+    $w = $info['width'];
+    $h = $info['height'];
+
+    $pixels = [];
+
+    $img = imagecreatefrompng($filename);
+    for ($x = 0; $x < $w; $x++) {
+        for ($y = 0; $y < $h; $y++) {
+
+            $pixels[$y * $w + $x] = imagecolorat($img, $x, $y);
+        }
+    }
+
+    imagedestroy($img);
+
+    return $pixels;
+}
+
+function saveAs($filename, $pixels, $info) {
+    $w = $info['width'];
+    $h = $info['height'];
+
+    $img = @imagecreatetruecolor($w, $h) or die("Cannot Initialize new GD image stream");
+
+    for ($i = 0; $i < count($pixels);) {
+        $rgb = $pixels[$i];
+        $y = ceil($i / $w) - 1;
+        $x = $i % $w;
+
+        imagesetpixel($img, $x, $y, $rgb);
+        $i++;
+    }
+
+    imagepng($img, $filename);
+    imagedestroy($img);
+}
+
+/**
+ * http://www.tannerhelland.com/3643/grayscale-image-algorithm-vb6/
+ * 
+ * @param type $pixels
+ * @param type $info
+ * @param type $algorithm
+ * @param type $shades
+ * @return type
+ */
+function grayScale($pixels, $info, $algorithm = 'luminosity', $shades = 32) {
+
+    $result = [];
+
+    $img = imagecreatetruecolor(10, 10) or die("Cannot Initialize new GD image stream");
+
+    for ($i = 0; $i < count($pixels); $i++) {
+        $RGB = $pixels[$i];
+
+        $R = ($RGB >> 16) & 0xFF;
+        $G = ($RGB >> 8) & 0xFF;
+        $B = $RGB & 0xFF;
+
+        switch ($algorithm) {
+            case 'shades':
+                $factor = 255 / ($shades - 1);
+                $avg = array_sum([$R, $G, $B]) / 3;
+                $gray = intval(($avg / $factor) + 0.5) * $factor;
+                break;
+            
+            case 'lightness':
+                $gray = (max([$R, $G, $B]) + min([$R, $G, $B])) / 2;
+                break;
+            
+            case 'max':
+                $gray = max([$R, $G, $B]);
+                break;
+            case 'min':
+                $gray = min([$R, $G, $B]);
+                break;
+            case 'average':
+                $gray = array_sum([$R, $G, $B]) / 3;
+                break;
+            
+            case 'red':
+                $gray = $R;
+                break;
+            case 'green':
+                $gray = $G;
+                break;
+            case 'blue':
+                $gray = $B;
+                break;
+            
+            case 'luma':
+                $gray = array_sum([0.299 * $R, 0.587 * $G, 0.114 * $B]);
+                break;
+            case 'luminosity':
+                $gray = array_sum([0.2126 * $R, 0.7152 * $G, 0.0722 * $B]);
+                break;
+            
+            default:
+                $gray = 0;
+                break;
+        }
+
+        $gray = intval($gray);
+
+        $result[] = imagecolorallocate($img, $gray, $gray, $gray);
+    }
+
+    imagedestroy($img);
+
+    return $result;
+}
+
+$info = getInfo($filename);
+$pixels = getPixels($filename);
+foreach ([
+    'lightness', 'min', 'max', 'average', 'luma', 'luminosity', 'dummy', 'red', 'green', 'blue', 'shades'] as $algorithm) {
+    var_dump($algorithm);
+    $result = grayScale($pixels, $info, $algorithm);
+    saveAs($algorithm . '.png', $result, $info);
+}
+
